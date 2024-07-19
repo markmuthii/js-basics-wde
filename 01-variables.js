36b6149bdf72511f5d3d909904bbb98abbe5ddb7
Mark Muthii <mark.muthii@strathmore.edu>
2024-07-19 23:24:29 +0300

    Variables and Arithmetic operators

---
+++ 01-variables.js
@@ -0,0 +1,26 @@
+// Declaration and Initialization (Assigning)
+//  - var
+var variableOne = "this is variable one"; // declaring and initializing variableOne using var keyword
+// console.log(variableOne);
+//  - let
+let variableTwo; // declaring variableTwo using the let keyword
+
+variableTwo = 77; // initializing (assigning a value to) variableTwo
+// console.log(variableTwo);
+
+//  - const
+const variableThree = true; // when using const, you have to declare and initialize the  variable in the same expression (line of code)
+console.log(variableThree);
+
+const variableFour = false;
+
+// Reassigning
+// variable declared with the let and var keywords can be reassigned, but variable declared with the const keyword cannot
+variableOne = "variableOne has been updated (reassigned)";
+// console.log(variableOne);
+variableTwo = "variableTwo has also been updated";
+// console.log(variableTwo);
+
+variableThree = "something else"; // this will throw an error because you cannot reassign values to constant variables
+// console.log(variableThree);
+// console.log(variableTwo);
---
+++ 02-arithmetic-operators.js
@@ -0,0 +1,28 @@
+let num1 = 43;
+let num2 = 67 + 10;
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 / num2);
+
+// ++ adds one to the value stored in the variable
+console.log(num1); // 43
+// // num1 = num1 + 1;
+// // num1 += 1;
+// // let postIncrement = num1++;
+// let preIncrement = ++num1;
+
+// // console.log(postIncrement);
+// console.log(preIncrement);
+// console.log(num1);
+
+// INCREMENT
+// console.log(num1++); // post increment // 43
+// console.log(num1); // 44
+// console.log(++num1); // pre increment // 45
+
+// DECREMENT
+console.log(num1--); // post decrement // 43
+console.log(num1); // 42
+console.log(--num1); // pre decrement // 41
